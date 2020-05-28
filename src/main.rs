4b9879db8be787d090b8cb8031467dce7e6f11ba
Theodore L <theodoreleebrant@gmail.com>
2020-05-28 17:50:37 +0800

    Moved CPU to own file

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,10 @@
+const CHIP8_WIDTH: usize = 64;
+const CHIP8_HEIGHT: usize = 32;
+const CHIP8_RAM: usize = 4096;
+
 fn main() {
-    let cpu = CPU {
-    	opcode: 0,
-    	v: [0; 16],
-    	i: 0x200,
-    	sound_timer: 0,
-    	delay_timer: 0,
-    	pc: 0x200,
-    	sp: 0,
-    	memory: [0;4096]
-    };
+    // legacy
+    // let cpu = CPU::new();
+    // cpu.load_program(vec![0x13, 0xC5]); // Loads a program which tells it to do 13C5 - see opcode 1NNN
 }
-
-struct CPU {
-	opcode: u16,			// opcodes
-	v: [u8; 16],			// registers v0, v1, ..., vF; 8-bit
-	i: u16,					// register i; 16-bit for memory address
-	sound_timer: u8,		// Sound timer; 8-bit
-	delay_timer:u8,			// Delay time: 8-bit
-	pc: u16,				// Program counter 
-	sp: u8,					// Stack pointer
-	memory: [u8; 4096]		// Memory, 4kB
-}
-
 
