038d975d13a27e72e107a9d6f79560768f4cd5f7
mgiang2015 <mgiang2015@gmail.com>
2020-05-28 22:39:28 +0800

    Include input driver in mod.rs now

--- src/drivers/mod.rs
+++ src/drivers/mod.rs
@@ -2,10 +2,10 @@
 // Enable those that you have done
 mod graphic_driver;
 //mod audio_driver;
-//mod input_driver;
+mod input_driver;
 mod cartridge_driver;
 
 pub use self::graphic_driver::Graphicriver;
 //pub use self::audio_driver::AudioDriver;
-//pub use self::input_driver::InputDriver;
+pub use self::input_driver::InputDriver;
 pub use self::cartridge_driver::CartridgeDriver;
