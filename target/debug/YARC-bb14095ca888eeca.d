1bc90d32fe50f43adedfa57c0a24549d3971f44b
mgiang2015 <mgiang2015@gmail.com>
2020-05-28 22:53:14 +0800

    Added tests that were ripped off

---
+++ target/debug/YARC-bb14095ca888eeca.d
@@ -0,0 +1,1 @@
+/Users/min/Git/YARC/target/debug/YARC-bb14095ca888eeca: /Users/min/Git/YARC/src/main.rs
---
+++ target/debug/deps/YARC-bb14095ca888eeca.d
@@ -0,0 +1,5 @@
+/Users/min/Git/YARC/target/debug/deps/YARC-bb14095ca888eeca: src/main.rs
+
+/Users/min/Git/YARC/target/debug/deps/YARC-bb14095ca888eeca.d: src/main.rs
+
+src/main.rs:
