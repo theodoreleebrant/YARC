3943fdea1d810b8a9a8591de154075b7e13e566e
Theodore L <theodoreleebrant@gmail.com>
2020-05-28 22:46:03 +0800

    amended module file to include all modules

--- src/drivers/mod.rs
+++ src/drivers/mod.rs
@@ -1,11 +1,11 @@
 // This is the module definition file
 // Enable those that you have done
 mod graphic_driver;
-//mod audio_driver;
+mod audio_driver;
 mod input_driver;
 mod cartridge_driver;
 
 pub use self::graphic_driver::Graphicriver;
-//pub use self::audio_driver::AudioDriver;
+pub use self::audio_driver::AudioDriver;
 pub use self::input_driver::InputDriver;
 pub use self::cartridge_driver::CartridgeDriver;
