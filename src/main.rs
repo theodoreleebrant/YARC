20b8e1e7f87bc8d42f2088d8d2d79f195ac0ba24
Theodore L <theodoreleebrant@gmail.com>
2020-05-28 17:17:20 +0800

    CPU Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let cpu = CPU {
+    	opcode: 0,
+    	v: [0; 16],
+    	i: 0x200,
+    	sound_timer: 0,
+    	delay_timer: 0,
+    	pc: 0x200,
+    	sp: 0,
+    	memory: [0;4096]
+    };
 }
+
+struct CPU {
+	opcode: u16,			// opcodes
+	v: [u8; 16],			// registers v0, v1, ..., vF; 8-bit
+	i: u16,					// register i; 16-bit for memory address
+	sound_timer: u8,		// Sound timer; 8-bit
+	delay_timer:u8,			// Delay time: 8-bit
+	pc: u16,				// Program counter 
+	sp: u8,					// Stack pointer
+	memory: [u8; 4096]		// Memory, 4kB
+}
+
+
